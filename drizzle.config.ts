8f0d60fb3973e977af7437fb612565bbdbd90ffe
Anwar3006 <anwarsadat.d2@gmail.com>
2025-12-16 23:42:34 +0000

    chore: added biome script to package.json - tested format script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 
-console.log("DB_URL: ", process.env.DATABASE_URL);
-
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/v1/schema.ts", //modified to point to the schema file that exports all models
   casing: "snake_case",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL as string,
   },
 });
